2d0c5841e046fc1c630442945a59de5a8a384957
Benjamin J. Young <ben@blaher.me>
2024-01-07 23:36:00 -0500

    Setting up directories

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# avcorn-templates-coffeeshop
-AVCorn Coffeeshop Template
+# AVCorn Coffeeshop Template
+
+![Template Preview](assets/images/previews/preview-lg.png "Coffeeshop Design Preview")
+
+A template for a coffeeshop client.
+
+
+
+[< Back to AVCorn Templates](https://github.com/AVCorn/avcorn-templates)
+
+[<< Back to AVCorn Project](https://github.com/AVCorn/avcorn)
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * New Template Configuration
+ *
+ * PHP version 8.2
+ *
+ * @phpversion >= 8.2
+ * @category   CMS
+ * @package    AVCorn
+ * @subpackage Template
+ * @author     Benjamin J. Young <ben@blaher.me>
+ * @license    GNU General Public License, version 3
+ * @link       https://github.com/avcorn/avcorn
+ */
+
+declare(strict_types=1);
+
+/**
+ * Overriding default configuration.
+ *
+ * @var array $config Configuration
+ */
+$config['info']['company'] = "Bene's Brew House";
+$config['info']['title'] = 'Crazy Good Coffee';
+$config['info']['motto'] = 'Get your fix!';
+$config['info']['slogan'] = 'Get your fix!';
+$config['info']['logo'] = '/assets/images/logos/logo-coffee.png';
